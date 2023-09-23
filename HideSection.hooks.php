894170c6b79eef71cf27202d26ed6c5a22519e32
paladox <paladox@users.noreply.github.com>
2023-09-23 14:14:03 +0100

    Update HideSection.hooks.php

--- HideSection.hooks.php
+++ HideSection.hooks.php
@@ -9,31 +9,31 @@ class HideSectionHooks {
     public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
         $out->addModules( 'ext.hideSection' );
         return true;
-     }
+    }
 
     public static function onSkinEditSectionLinks( $skin, $title, $section, $tooltip, &$links, $lang ) {
         global $wgHideSectionHideText;
 
-        if ($wgHideSectionHideText) return true;
+        if ( $wgHideSectionHideText ) return true;
 
         $hidetext  = wfMessage( 'hidesection-hide' )->text();
         $showtext  = wfMessage( 'hidesection-show' )->text();
         $titletext = wfMessage( 'hidesection-hidetitle' )->text();
 
         // Add hide/show link next to edit links
-        if ($section !== 0) {
+        if ( $section !== 0 ) {
             $links['hidesection'] = [
                 'targetTitle' => $title,
                 'text' => $hidetext,
                 'attribs' => [
-                    "class" => "hidesection-link internal",
-                    "data-show" => $showtext,
-                    "data-hide" => $hidetext,
-                    "data-section" => $section,
-                    "title" => $titletext,
-                    ],
-                'query' => array(),
-                'options' => array(),
+                    'class' => 'hidesection-link internal',
+                    'data-show' => $showtext,
+                    'data-hide' => $hidetext,
+                    'data-section' => $section,
+                    'title' => $titletext,
+                ],
+                'query' => [],
+                'options' => [],
             ];
         }
 
@@ -67,7 +67,7 @@ class HideSectionHooks {
             $hideall  = wfMessage( 'hidesection-hideall' )->text();
             $titleall = wfMessage( 'hidesection-hidealltitle' )->text();
 
-            $linkelem = Html::element('a', [
+            $linkelem = Html::element( 'a', [
                     'class' => 'hidesection-all internal',
                     'data-show' => $showall,
                     'data-hide' => $hideall,
@@ -76,8 +76,8 @@ class HideSectionHooks {
                 ],
                 $hideall
             );
-            $hideelem = Html::Rawelement('span',
-                 array( 'class' => 'hidesection-head' ),
+            $hideelem = Html::Rawelement( 'span',
+                 [ 'class' => 'hidesection-head' ],
                 '[' . $linkelem . ']'
             );
 
@@ -87,9 +87,8 @@ class HideSectionHooks {
     		}
 
             // Append to page title
-            $out->setPageTitle( $title . $hideelem );
+            $out->setPageTitle( $title . ' ' . $hideelem );
         }
         return true;
     }
 }
-
